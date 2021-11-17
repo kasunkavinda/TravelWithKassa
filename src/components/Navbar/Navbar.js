35f182e5a278dfcaba7fa83bacac45a74a638216
Kasun <kasunkavinda.bc@gmail.com>
2021-11-17 16:53:45 +0530

    new component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <Link to="/">
+          <div>ksksk</div>
+        </Link>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
