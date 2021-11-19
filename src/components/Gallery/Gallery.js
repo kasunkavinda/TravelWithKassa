e33623c9d1cea4066c31ba3e3170b8514400d2e8
Kasun <kasunkavinda.bc@gmail.com>
2021-11-19 22:37:03 +0530

    gallery iteration

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,47 +1,32 @@
 import React from "react";
 import classes from "./Gallery.module.css";
 
+const imageLists = [
+  { id: 1, imageUrl: "url(/images/gallery-images/1.jpg)" },
+  { id: 2, imageUrl: "url(/images/gallery-images/2.jpg)" },
+  { id: 3, imageUrl: "url(/images/gallery-images/3.jpg)" },
+  { id: 4, imageUrl: "url(/images/gallery-images/4.jpg)" },
+  { id: 5, imageUrl: "url(/images/gallery-images/5.jpg)" },
+];
+
 function Gallery() {
   return (
     <div className="container mx-auto">
-      <div className="grid md:grid-cols-3">
-        <div
-          className={`${classes.galleryImage} m-4`}
-          style={{
-            backgroundImage: "url(/images/gallery-images/1.jpg)",
-          }}
-        ></div>
-        <div
-          className={`${classes.galleryImage} m-4`}
-          style={{
-            backgroundImage: "url(/images/gallery-images/2.jpg)",
-          }}
-        ></div>
-        <div
-          className={`${classes.galleryImage} m-4`}
-          style={{
-            backgroundImage: "url(/images/gallery-images/3.jpg)",
-          }}
-        ></div>
-        <div
-          className={`${classes.galleryImage} m-4`}
-          style={{
-            backgroundImage: "url(/images/gallery-images/4.jpg)",
-          }}
-        ></div>
-        <div
-          className={`${classes.galleryImage} m-4`}
-          style={{
-            backgroundImage: "url(/images/gallery-images/5.jpg)",
-          }}
-        ></div>
-        <div
-          className={`${classes.galleryImage} m-4`}
-          style={{
-            backgroundImage: "url(/images/gallery-images/6.jpg)",
-          }}
-        ></div>
-      </div>
+      {imageLists && imageLists.length > 0 && (
+        <div className="grid md:grid-cols-3">
+          {imageLists.map((imageList) => {
+            return (
+              <div
+                key={imageList.id}
+                className={`${classes.galleryImage} m-4`}
+                style={{
+                  backgroundImage: imageList.imageUrl,
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
